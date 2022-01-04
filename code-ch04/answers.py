1143f7e0d931dad28555b3120bcbfff79f1e83e3
Robert Serrano <rserranon@gmail.com>
2022-01-03 18:44:13 -0600

    excercises chapter 1, 2 & 3

--- code-ch04/answers.py
+++ code-ch04/answers.py
@@ -1,28 +1,28 @@
-'''
+
 # tag::exercise1[]
-==== Exercise 1
+#==== Exercise 1
 
-Find the uncompressed SEC format for the public key where the private key secrets are:
+# Find the uncompressed SEC format for the public key where the private key secrets are:
 
-* 5,000
-* 2,018^5^
-* 0xdeadbeef12345
+#* 5,000
+#* 2,018^5^
+#* 0xdeadbeef12345
 # end::exercise1[]
 # tag::answer1[]
->>> from ecc import PrivateKey
->>> priv = PrivateKey(5000)
->>> print(priv.point.sec(compressed=False).hex())
-04ffe558e388852f0120e46af2d1b370f85854a8eb0841811ece0e3e03d282d57c315dc72890a4\
-f10a1481c031b03b351b0dc79901ca18a00cf009dbdb157a1d10
->>> priv = PrivateKey(2018**5)
->>> print(priv.point.sec(compressed=False).hex())
-04027f3da1918455e03c46f659266a1bb5204e959db7364d2f473bdf8f0a13cc9dff87647fd023\
-c13b4a4994f17691895806e1b40b57f4fd22581a4f46851f3b06
->>> priv = PrivateKey(0xdeadbeef12345)
->>> print(priv.point.sec(compressed=False).hex())
-04d90cd625ee87dd38656dd95cf79f65f60f7273b67d3096e68bd81e4f5342691f842efa762fd5\
-9961d0e99803c61edba8b3e3f7dc3a341836f97733aebf987121
-
+from ecc import PrivateKey
+priv = PrivateKey(5000)
+print(priv.point.sec(compressed=False).hex())
+#04ffe558e388852f0120e46af2d1b370f85854a8eb0841811ece0e3e03d282d57c315dc72890a4\
+#f10a1481c031b03b351b0dc79901ca18a00cf009dbdb157a1d10
+priv = PrivateKey(2018**5)
+print(priv.point.sec(compressed=False).hex())
+#04027f3da1918455e03c46f659266a1bb5204e959db7364d2f473bdf8f0a13cc9dff87647fd023\
+#c13b4a4994f17691895806e1b40b57f4fd22581a4f46851f3b06
+priv = PrivateKey(0xdeadbeef12345)
+print(priv.point.sec(compressed=False).hex())
+#04d90cd625ee87dd38656dd95cf79f65f60f7273b67d3096e68bd81e4f5342691f842efa762fd5\
+#9961d0e99803c61edba8b3e3f7dc3a341836f97733aebf987121
+'''
 # end::answer1[]
 # tag::exercise2[]
 ==== Exercise 2
